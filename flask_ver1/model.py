1c921ca1989a85e5bdf84ecd21ca9ae6114b8393
jaegeun <12zzzz12@naver.com>
2021-12-30 11:06:23 +0900

    KT_intern_03

---
+++ flask_ver1/model.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+# Import utilites
+from utils import label_map_util, visualization_utils as vis_util
+
+
+def modeling(img):
+    # pb 파일
+    PATH_TO_CKPT = 'frozen_inference_graph.pb'
+
+    # labelmap 파일
+    PATH_TO_LABELS = 'labelmap.pbtxt'
+
+    # 테스트 이미지
+    PATH_TO_IMAGE = img
+
+    NUM_CLASSES = 1
+
+    #
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    # Load the Tensorflow model into memory.
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+
+    # Input tensor is the image
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Output tensors are the detection boxes, scores, and classes
+    # Each box represents a part of the image where a particular object was detected
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represents level of confidence for each of the objects.
+    # The score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+    # Number of objects detected
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Load image using OpenCV and
+    # expand image dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    image = cv2.imread(PATH_TO_IMAGE)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_expanded = np.expand_dims(image_rgb, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=3,
+        min_score_thresh=0.60)
+    return image
+
+def imgreturn(img):
+    image = modeling(img)
+    cv2.imshow('Object detector', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return image
+
+imgreturn('test1.jpg')
+#
+# #결과 좌표구하는 코드
+#     min_score_thresh = .7
+#     for i in range(len(boxes[0])):
+#
+#         if scores is None or scores[0][i] > min_score_thresh:
+#             # boxes[i] is the box which will be drawn
+#             print( '1', scores[0][i], str(boxes[0][i]))
